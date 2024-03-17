380834cb5c1bd9be598ab236bef04785bd804cf2
ManuelDC81 <162871867+ManuelDC81@users.noreply.github.com>
2024-03-17 22:36:01 +0100

    Update Proyecto Actividad 2 Ciencia de Datos.py

--- Proyecto Actividad 2 Ciencia de Datos.py
+++ Proyecto Actividad 2 Ciencia de Datos.py
@@ -1,79 +1,135 @@
-# Importamos las bibliotecas necesarias
+# Importamos las librerías necesarias para el manejo de datos, solicitudes HTTP y visualización gráfica
 import pandas as pd
 import requests
 import plotly.graph_objs as go
 
-# Definimos una función para cargar datos desde un archivo Excel
-def cargar_datos_excel(ruta_archivo):
-    df = pd.read_excel(ruta_archivo)  # Cargar datos desde el archivo Excel
-    df['Año'] = pd.to_datetime(df['Año'], format='%Y').dt.year  # Convertir la columna de año al formato esperado
-    df.set_index('Año', inplace=True)  # Establecer la columna de año como índice
-    return df
-
-# Definimos una función para obtener datos desde una URL
+# Definimos una función para obtener datos desde una URL específica y convertirlos en un DataFrame de pandas
 def obtener_datos(url):
-    response = requests.get(url)  # Realizar una solicitud GET a la URL
-    if response.status_code == 200:  # Si la solicitud es exitosa
-        data = response.json()  # Convertimos los datos de respuesta a JSON
-        if len(data) > 1 and isinstance(data[1], list):  # Verificamos si los datos están en el formato esperado
-            df = pd.DataFrame(data[1])  # Creamos un DataFrame desde los datos
-            df = df[['date', 'value']]  # Seleccionamos solo las columnas relevantes
-            df.columns = ['Año', 'Valor']  # Renombramos las columnas
-            df['Año'] = pd.to_datetime(df['Año']).dt.year  # Convertimos la columna de año al formato esperado
-            df.set_index('Año', inplace=True)  # Establecemos la columna de año como índice
-            df.dropna(inplace=True)  # Eliminamos filas con valores nulos
-            return df  # Devolvemos el DataFrame con los datos
+    try:
+        # Realizamos una solicitud GET a la URL
+        response = requests.get(url)
+        # Verificamos que la respuesta sea exitosa (código de estado 200)
+        response.raise_for_status()
+        # Convertimos la respuesta en formato JSON a un diccionario de Python
+        data = response.json()
+        # Verificamos si hay datos disponibles y si el segundo elemento es una lista
+        if len(data) > 1 and isinstance(data[1], list):
+            # Creamos un DataFrame de pandas con los datos obtenidos
+            df = pd.DataFrame(data[1])
+            # Seleccionamos y renombramos las columnas de interés
+            df = df[['date', 'value']]
+            df.columns = ['Año', 'Valor']
+            # Convertimos la columna 'Año' al formato de fecha y extraemos solo el año
+            df['Año'] = pd.to_datetime(df['Año']).dt.year
+            # Establecemos la columna 'Año' como el índice del DataFrame
+            df.set_index('Año', inplace=True)
+            # Eliminamos filas con valores faltantes
+            df.dropna(inplace=True)
+            return df
         else:
-            print("No hay datos disponibles en la respuesta")  # Imprimir un mensaje de error si no hay datos disponibles
-            return pd.DataFrame()  # Devolvemos un DataFrame vacío
-    else:
-        print("Fallo al obtener datos de la API del Banco Mundial")  # Imprimir un mensaje de error si la solicitud falla
-        return pd.DataFrame()  # Devolvemos un DataFrame vacío
+            print("No hay datos disponibles en la respuesta")
+            return pd.DataFrame()
+    except requests.RequestException as e:
+        # Manejamos cualquier error en la solicitud HTTP
+        print(f"Error al obtener datos: {e}")
+        return pd.DataFrame()
+    
+# Definimos una función para cargar datos de múltiples URLs, donde cada URL corresponde a un indicador diferente
+def cargar_datos(urls):
+    dfs = {}
+    for key, url in urls.items():
+        dfs[key] = obtener_datos(url)
+    return dfs
 
-# Definimos la ruta del archivo Excel que contiene los datos de delincuencia
-ruta_archivo_delincuencia = 'C:/Users/Usuario/Documents/CIENCIA DE DATOS/DELINCUENCIA EN EL SALVADOR 20 AÑOS.xlsx'
-# Cargamos los datos de delincuencia desde el archivo Excel
-df_delincuencia = cargar_datos_excel(ruta_archivo_delincuencia)
+# Diccionario que contiene las URLs (APIs) de los indicadores de interés para El Salvador
+urls = {
+    "PIB": "http://api.worldbank.org/v2/country/SLV/indicator/NY.GDP.MKTP.CD?date=1994:2024&format=json",
+    "Crecimiento económico" : "http://api.worldbank.org/v2/country/SLV/indicator/NY.GDP.MKTP.KD.ZG?date=1994:2024&format=json",
+    "Inflación" : "http://api.worldbank.org/v2/country/SLV/indicator/FP.CPI.TOTL.ZG?date=1994:2024&format=json",
+    "Tasa de desempleo" : "http://api.worldbank.org/v2/country/SLV/indicator/SL.UEM.TOTL.ZS?date=1994:2024&format=json",
+    "Gasto Público" : "http://api.worldbank.org/v2/country/SLV/indicator/GC.XPN.TOTL.GD.ZS?date=1994:2024&format=json",
+    "Nivel de pobreza" : "http://api.worldbank.org/v2/country/SLV/indicator/SI.POV.DDAY?date=1994:2024&format=json",
+    "Mortalidad niños < 5 años" : "http://api.worldbank.org/v2/country/SLV/indicator/SI.POV.DDAY?date=1994:2024&format=json",
+    "Distribución de los ingresos" : "http://api.worldbank.org/v2/country/SLV/indicator/SI.POV.GINI?date=1994:2024&format=json",
+    "Tasa Alfabetización" : "http://api.worldbank.org/v2/country/SLV/indicator/SE.ADT.LITR.ZS?date=1994:2024&format=json",
+    "Mujeres en el parlamento" : "http://api.worldbank.org/v2/country/SLV/indicator/SG.GEN.PARL.ZS?date=1994:2024&format=json",
+    "Homicidios<br>(por cada 100.000 hab)" : "http://api.worldbank.org/v2/country/SLV/indicator/VC.IHR.PSRC.P5?date=1994:2024&format=json",
+    "Emisiones de carbono" : "http://api.worldbank.org/v2/country/SLV/indicator/EN.ATM.CO2E.KT?date=1994:2024&format=json",
+    "Acceso a la electricidad" : "http://api.worldbank.org/v2/country/SLV/indicator/EG.ELC.ACCS.ZS?date=1994:2024&format=json",
+    "Electricidad provenientes de<br>fuentes de energías renovable" : "http://api.worldbank.org/v2/country/SLV/indicator/EG.ELC.RNWX.KH?date=1994:2024&format=json",
+    "Acceso a Internet" : "http://api.worldbank.org/v2/country/SLV/indicator/IT.NET.USER.ZS?date=1994:2024&format=json",
+    "Uso telefonía móvil" : "http://api.worldbank.org/v2/country/SLV/indicator/IT.CEL.SETS.P2?date=1994:2024&format=json",
+    "Uso Banda Ancha" : "http://api.worldbank.org/v2/country/SLV/indicator/IT.NET.BBND.P2?date=1994:2024&format=json"
+}
 
-# Definimos las URLs para los indicadores económicos, ambientales y de delincuencia
-url_pib = "http://api.worldbank.org/v2/country/SLV/indicator/NY.GDP.MKTP.CD?date=1994:2024&format=json"
-url_crecimiento_economico = "http://api.worldbank.org/v2/country/SLV/indicator/NY.GDP.MKTP.KD.ZG?date=1994:2024&format=json"
-url_inflacion = "http://api.worldbank.org/v2/country/SLV/indicator/FP.CPI.TOTL.ZG?date=1994:2024&format=json"
-url_desempleo = "http://api.worldbank.org/v2/country/SLV/indicator/SL.UEM.TOTL.ZS?date=1994:2024&format=json"
-url_gasto_publico = "http://api.worldbank.org/v2/country/SLV/indicator/GC.XPN.TOTL.GD.ZS?date=1994:2024&format=json"
-url_nivel_pobreza = "http://api.worldbank.org/v2/country/SLV/indicator/SI.POV.DDAY?date=1994:2024&format=json"
-url_tasa_alfabetizacion = "http://api.worldbank.org/v2/country/SLV/indicator/SE.ADT.LITR.ZS?date=1994:2024&format=json"
-url_emisiones_carbono = "http://api.worldbank.org/v2/country/SLV/indicator/EN.ATM.CO2E.KT?date=1994:2024&format=json"
+# Mapeamos cada indicador a un Objetivo de Desarrollo Sostenible (ODS) específico
+ods_correspondencia = {
+    "pib": "ODS 8: Trabajo decente y crecimiento económico",
+    "Crecimiento económico": "ODS 8: Trabajo decente y crecimiento económico",
+    "inflacion": "ODS 8: Reducción de las desigualdades",
+    "desempleo": "ODS 8: Trabajo decente y crecimiento económico",
+    "gasto_publico": "ODS 8: Alianzas para lograr los objetivos",
+    "Nivel de pobreza" : "ODS 1: Fin de la pobreza",
+    "mortalidad_ninos" : "ODS 3: Salud y bienestar",
+    "gini" : "ODS 10: Reducción de las desigualdades",
+    "tasa_alfabetizacion" : "ODS 4: Educación de calidad",
+    "mujeres_parlamento" : "ODS 5: Igualdad de género",
+    "delincuencia_100" : "ODS 16: Paz, justicia e instituciones sólidas",
+    "emisiones_carbono" : "ODS 13: Acción por el clima",
+    "acceso_electicidad" : "ODS 9: Industria, innovación e infraestructura",
+    "elect_renovable" : "ODS 9: Industria, innovación e infraestructura",
+    "uso_internet" : "ODS 17: Alianzas para los objetivos",
+    "uso_telefono_movil" : "ODS 17: Alianzas para los objetivos",
+    "uso_banda_ancha" : "ODS 17: Alianzas para los objetivos"
+}
 
-# Obtenemos los datos de los indicadores económicos, ambientales y de delincuencia desde las URLs
-df_pib = obtener_datos(url_pib)
-df_crecimiento_economico = obtener_datos(url_crecimiento_economico)
-df_inflacion = obtener_datos(url_inflacion)
-df_desempleo = obtener_datos(url_desempleo)
-df_gasto_publico = obtener_datos(url_gasto_publico)
-df_nivel_pobreza = obtener_datos(url_nivel_pobreza)
-df_tasa_alfabetizacion = obtener_datos(url_tasa_alfabetizacion)
-df_emisiones_carbono = obtener_datos(url_emisiones_carbono)
+# Cargamos los datos utilizando las URLs definidas anteriormente
+dfs = cargar_datos(urls)
 
-# Creamos un gráfico con Plotly
+# Ordenamos los ODS de menor a mayor
+ods_ordenados = [
+    "ODS 1: Fin de la pobreza",
+    "ODS 3: Salud y bienestar",
+    "ODS 4: Educación de calidad",
+    "ODS 5: Igualdad de género",
+    "ODS 8: Trabajo decente y crecimiento económico",
+    "ODS 9: Industria, innovación e infraestructura",
+    "ODS 10: Reducción de las desigualdades",
+    "ODS 13: Acción por el clima",
+    "ODS 16: Paz, justicia e instituciones sólidas",
+    "ODS 17: Alianzas para los objetivos"
+]
+
+# Agrupamos los indicadores según el ODS al que pertenecen
+ods_agrupados = {
+    "ODS 1: Fin de la pobreza": ["Nivel de pobreza"],
+    "ODS 3: Salud y bienestar": ["Mortalidad niños < 5 años"],
+    "ODS 4: Educación de calidad": ["Tasa Alfabetización"],
+    "ODS 5: Igualdad de género": ["Mujeres en el parlamento"],
+    "ODS 8: Trabajo decente y crecimiento económico": ["PIB", "Crecimiento económico", "Inflación", "Tasa de desempleo","Gasto Público"],
+    "ODS 9: Industria, innovación e infraestructura": ["Acceso a la electricidad", "Electricidad provenientes de<br>fuentes de energías renovables"],
+    "ODS 10: Reducción de las desigualdades": ["Distribución de los ingresos"],
+    "ODS 13: Acción por el clima": ["Emisiones de carbono"],
+    "ODS 16: Paz, justicia e instituciones sólidas": ["Homicidios<br>(por cada 100.000 hab)"],
+    "ODS 17: Alianzas para los objetivos": ["Acceso a Internet", "Uso telefonía móvil", "Uso Banda Ancha"]
+}
+
+# Creamos una figura vacía para la visualización de los datos
 fig = go.Figure()
 
-# Agregamos las series al gráfico
-fig.add_trace(go.Scatter(x=df_pib.index, y=df_pib['Valor'], mode='lines+markers', name='PIB'))
-fig.add_trace(go.Scatter(x=df_crecimiento_economico.index, y=df_crecimiento_economico['Valor'], mode='lines+markers', name='Crecimiento Económico'))
-fig.add_trace(go.Scatter(x=df_inflacion.index, y=df_inflacion['Valor'], mode='lines+markers', name='Inflación'))
-fig.add_trace(go.Scatter(x=df_desempleo.index, y=df_desempleo['Valor'], mode='lines+markers', name='Tasa de Desempleo'))
-fig.add_trace(go.Scatter(x=df_gasto_publico.index, y=df_gasto_publico['Valor'], mode='lines+markers', name='Gasto Público'))
-fig.add_trace(go.Scatter(x=df_nivel_pobreza.index, y=df_nivel_pobreza['Valor'], mode='lines+markers', name='Nivel de Pobreza'))
-fig.add_trace(go.Scatter(x=df_tasa_alfabetizacion.index, y=df_tasa_alfabetizacion['Valor'], mode='lines+markers', name='Tasa de Alfabetización'))
-fig.add_trace(go.Scatter(x=df_emisiones_carbono.index, y=df_emisiones_carbono['Valor'], mode='lines+markers', name='Emisiones de CO2'))
-fig.add_trace(go.Scatter(x=df_delincuencia.index, y=df_delincuencia['Homicidios'], mode='lines+markers', name='Tasa de Homicidios'))
+# Añadimos las trazas según la agrupación de ODS
+for ods_nombre, indicadores in ods_agrupados.items():
+    for indicador in indicadores:
+        df = dfs.get(indicador)
+        if df is not None:
+            fig.add_trace(go.Scatter(x=df.index, y=df['Valor'], mode='lines+markers',
+                                     name=f"{ods_nombre} - {indicador}", visible="legendonly"))
 
-# Configuramos el diseño del gráfico
-fig.update_layout(title="Indicadores de El Salvador: Económicos, Ambientales y de Delincuencia",
+# Actualizamos la configuración del layout del gráfico, incluyendo títulos y leyendas
+fig.update_layout(title="Indicadores de El Salvador: Socio-Económicos, Ambientales y de Delincuencia",
                   xaxis_title='Año',
                   yaxis_title='Valor',
-                  legend_title="Indicadores")
+                  legend_title="Indicadores y ODS correspondientes")
 
-# Mostramos el gráfico
+# Mostramos la gráfica
 fig.show()
